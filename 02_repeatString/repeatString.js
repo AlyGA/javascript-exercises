e13c3a614ea4e183ea1763a844f446ca0e886cca
Aly Anderson <aly779372@gmail.com>
2022-03-01 14:04:13 -0800

    Create solution for repeat string problem

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+    for (i = 1; i <= number; i++) {
+        return word.repeat(number);
+    }
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
