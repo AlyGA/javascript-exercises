cebf8569e667938f41723ac9fa6d4fd748c33992
Aly Anderson <aly779372@gmail.com>
2022-03-03 13:11:47 -0800

    Add solution for reversing the string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function (phrase) {
+  return phrase.split("").reverse().join("");
+  // This first splits any phrase of choice into a string, then reverses the array's order, and then joins it back together.
 };
 
 // Do not edit below this line
