207494a86699e1b2b404b606bc5269e3967cef7e
Aly Anderson <aly779372@gmail.com>
2022-03-06 14:48:19 -0800

    Add function to convert fahrenheit and celsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,28 @@
-const ftoc = function() {
-
+"use strict";
+const ftoc = function (temp) {
+  let modifiedTemp = ((temp - 32) * 5) / 9;
+  if (modifiedTemp % 1 !== 0) {
+    return Number(modifiedTemp.toFixed(1));
+  } else if (modifiedTemp % 1 === 0) {
+    return Number(modifiedTemp);
+  } else {
+    return "Invalid input!";
+  }
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  let modifiedTemp = (temp * 9) / 5 + 32;
+  if (modifiedTemp % 1 !== 0) {
+    return Number(modifiedTemp.toFixed(1));
+  } else if (modifiedTemp % 1 === 0) {
+    return Number(modifiedTemp);
+  } else {
+    return "Invalid input!";
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
