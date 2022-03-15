bec63fb6167caecb4b218ec91450b32480601fb3
Aly Anderson <aly779372@gmail.com>
2022-03-15 13:34:42 -0700

    Add solution so that calculator can work

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (a) {
+  const initialValue = 0;
+  const sumWithInitial = a.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    initialValue
+  );
+  return sumWithInitial;
 };
 
-const multiply = function() {
-
+const multiply = function (a) {
+  const sumWithInitial = a.reduce(
+    (previousValue, currentValue) => previousValue * currentValue
+  );
+  return sumWithInitial;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let answer = 1;
+  if (a === 0 || a === 1) {
+    return answer;
+  } else {
+    for (let i = a; i >= 1; i--) {
+      answer = answer * i;
+    }
+    return answer;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +45,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
