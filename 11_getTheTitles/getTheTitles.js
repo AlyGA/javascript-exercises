955a560a16f955c19622fbf5def72eb2639d6df8
Aly Anderson <aly779372@gmail.com>
2022-03-16 11:09:21 -0700

    Add solution to retrieve only the titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (book) {
+  let bookArray = []; // Empty array to place items
+  book = book.map((book) => book.title); // Filter and return only the values of "title"
+  bookArray = book; // Add those values to the new array
+  return bookArray; // Return the array
 };
 
 // Do not edit below this line
