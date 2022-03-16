107f0b06547555cdef7a4fd0c8d9cd5434cbf439
Aly Anderson <aly779372@gmail.com>
2022-03-16 09:45:38 -0700

    Add solution for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  //1. replace every non-word character and underscore with an empty string.
+  let replace = /[\W_]/g;
 
+  // Lowercase all the words and replace with the new word pattern.
+  let wordLowerCase = word.toLowerCase().replace(replace, "");
+
+  // Reverse the word
+  const reverseWord = wordLowerCase.split("").reverse().join("");
+
+  // Compare if they are the same or not
+  return wordLowerCase === reverseWord;
 };
 
 // Do not edit below this line
